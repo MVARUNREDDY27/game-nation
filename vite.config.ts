a39c56b1b21891596878d8fb8ecc0868d16990a9
M VARUN REDDY <153171908+MVARUNREDDY27@users.noreply.github.com>
2026-06-17 11:35:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base : '/game-nation/'
+  base: '/game-nation/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
